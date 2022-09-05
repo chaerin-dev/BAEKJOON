69a9395976115565b178ad6f5fec44d5d9ac78ac
chaerin-dev <chaerin.dev@gmail.com>
2022-09-05 10:15:29 +0900

    Update 2609. 최대공약수와 최소공배수.js

--- BRONZE1/2609. 최대공약수와 최소공배수.js
+++ BRONZE1/2609. 최대공약수와 최소공배수.js
@@ -5,12 +5,24 @@ const [N, M] = require("fs")
   .split(" ")
   .map((e) => +e);
 
-// 유클리드 호제법
-const gcd = (num1, num2) => (num2 ? gcd(num2, num1 % num2) : num1);
+// // 첫 번째 풀이: 브루트포스(통과)
+// function solution(N, M) {
+//   let min = Math.min(N, M);
+//   let gcd;
+//   for (let i = min; i > 0; i--) {
+//     if (N % i === 0 && M % i === 0) {
+//       gcd = i;
+//       break;
+//     }
+//   }
+//   return [gcd, (N * M) / gcd];
+// }
 
+// 두 번째 풀이: 유클리드 호제법 이용(통과)
+const gcd = (num1, num2) => (num2 ? gcd(num2, num1 % num2) : num1);
 function solution(N, M) {
   const gcdNM = gcd(N, M);
-  return [gcdNM, (N * M) / gcdNM].join("\n");
+  return [gcdNM, (N * M) / gcdNM];
 }
 
-console.log(solution(N, M));
+console.log(solution(N, M).join("\n"));
